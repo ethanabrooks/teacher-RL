cab4a562dde82cf8d5a206f2deefc3c63c4379bb
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-24 14:11:58 -0400

    prefix episode_

--- epoch_counter.py
+++ epoch_counter.py
@@ -29,8 +29,8 @@ class EpochCounter:
 
     def items(self, prefix=""):
         if self.episode_rewards:
-            yield prefix + "rewards", np.mean(self.episode_rewards)
+            yield prefix + "episode_rewards", np.mean(self.episode_rewards)
         if self.episode_time_steps:
-            yield prefix + "time_steps", np.mean(self.episode_time_steps)
+            yield prefix + "episode_time_steps", np.mean(self.episode_time_steps)
         for k, vs in self.infos.items():
             yield prefix + k, np.mean(vs)
