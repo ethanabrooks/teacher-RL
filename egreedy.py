9e7c35d770fe0d8f6be59127fa0bd5676a4cacaa
Ethan Brooks <ethanabrooks@gmail.com>
2020-08-29 08:41:28 -0400

    search bandit hyperparameters

--- egreedy.py
+++ egreedy.py
@@ -1,8 +1,10 @@
+import itertools
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 from gym.utils import seeding
+from tensorboardX import SummaryWriter
 
 """
 Created on Tue Sep 11 17:00:29 2018
@@ -46,20 +48,48 @@ class EGreedy:
             e = yield choice, reward
 
 
-def main():
+def main(log_dir: Path):
     # dataset = pd.read_csv("Ads_CTR_Optimisation.csv")
-    choices = 5
-    batches = 2
-    size = (batches, choices)
-    means = np.random.normal(size=size, scale=1)
-    # stds = abs(np.random.normal(size=size, scale=0.01))
-    T = 10
 
+    T = 1000
+    n = 1000
+    d = 10
+
+    means = np.zeros((n, d))
+    np.random.seed(0)
+    means[:, int(np.random.choice(d))] = 1
+    means = np.zeros((n, d))
+    np.random.seed(0)
+    means[:, int(np.random.choice(d))] = 1
     loc = np.tile(means, (T, 1, 1))
-    # scale = np.tile(stds, (T, 1, 1))
+    dataset = np.random.normal(loc, 2)
+    optimal = loc.max(axis=-1, initial=-np.inf)
 
     # dataset = np.random.binomial(choices, p)
-    dataset = np.random.normal(loc)
+    for initial_i in [0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+        for rate in [0.85, 0.9, 0.95, 0.99, 0.999, 0.9999]:
+            print("rate:", rate)
+            print("i_0:", initial_i)
+            path = Path(log_dir, str(initial_i), str(rate))
+            with SummaryWriter(logdir=str(path)) as writer:
+                bandit = EGreedy(0).train_loop(dataset)
+                next(bandit)
+                i = initial_i
+                for t in itertools.count():
+                    try:
+                        interaction = bandit.send(i)
+                    except StopIteration:
+                        print("t", t)
+                        print("i_t", i)
+                        break
+                    i *= rate
+                    choices, rewards = interaction
+                    chosen_means = loc[t, 0][choices.astype(int).flatten()].flatten()
+                    regret = optimal[t : t + 1] - chosen_means
+                    writer.add_scalar("reward", np.mean(rewards), t)
+                    writer.add_scalar("regret", np.mean(regret), t)
+                    writer.add_scalar("i", i, t)
+
     choices, rewards = [np.stack(x) for x in zip(*EGreedy().train_loop(dataset))]
 
     with Path("results.npz").open("wb") as f:
@@ -67,4 +97,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(Path("/tmp/log"))
