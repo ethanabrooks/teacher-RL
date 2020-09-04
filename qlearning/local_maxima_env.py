7e004a437f7bf09c3dd3f2a8dad92a66240d8434
Ethan Brooks <ethanabrooks@gmail.com>
2020-09-04 17:47:43 -0400

    add main() function to LocalMinimaEnv

--- qlearning/local_maxima_env.py
+++ qlearning/local_maxima_env.py
@@ -23,11 +23,12 @@ class Env(gym.Env):
         return self.iterator.send(action)
 
     def render(self, mode="human"):
-        pass
+        self._render()
 
     def generator(self):
         s = 0
         action = 0
+        R = 0
         for stage in itertools.count():
             for step in range(stage):
                 if action:
@@ -38,3 +39,27 @@ class Env(gym.Env):
                     t = False or (s + 1) == self.num_states
                 action = yield s, r, t, {}
                 s += 1
+
+
+def main():
+    env = Env(100)
+    env.reset()
+    _return = 0
+    while True:
+        env.render()
+        print("Return:", _return)
+        while True:
+            try:
+                a = int(input("go:"))
+                break
+            except ValueError:
+                pass
+        s, r, t, i = env.step(a)
+        _return += r
+        if t:
+            _return = 0
+
+
+if __name__ == "__main__":
+    PARSER = argparse.ArgumentParser()
+    main()
